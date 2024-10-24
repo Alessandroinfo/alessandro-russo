73d8430b52d384fc89f33dca4bd3400ccc5f1ca9
Alessandro Russo <alessandro.russo@datlas.it>
2024-10-24 15:28:01 +0200

    - Migrazione Next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,30 @@
 import "./globals.scss";
 import Head from 'next/head';
 import React from 'react';
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+    title: 'Alessandro Russo | Front-End Architect',
+    description: 'Alessandro Russo | Web Architect',
+    viewport: 'width=device-width, initial-scale=1',
+    openGraph: {
+        title: 'Alessandro Russo | Web Architect',
+        type: 'website',
+        url: 'https://www.alessandro-russo.com',
+        images: [
+            {
+                url: 'https://i.imgur.com/lxRBpeb.jpg',
+                alt: 'Alessandro Russo profile image',
+            },
+        ],
+        description: 'Alessandro Russo | Web Architect Portfolio.',
+    },
+    twitter: {
+        card: 'summary',
+        creator: '@Alessandro_Info',
+    },
+    themeColor: '#032a2e',
+};
 
 export default function RootLayout({
     children,
@@ -9,20 +33,6 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <Head>
-            <title>Alessandro Russo | Front-End Architect</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <meta name="description" content="Alessandro Russo | Web Architect"/>
-            <meta property="og:title" content="Alessandro Russo | Web Architect"/>
-            <meta property="og:type" content="portfolio"/>
-            <meta property="og:url" content="https://www.alessandro-russo.com"/>
-            <meta property="og:image" content="https://i.imgur.com/lxRBpeb.jpg"/>
-            <meta property="og:image:alt" content="Alessandro Russo profile image."/>
-            <meta property="og:description" content="Alessandro Russo | Web Architect Portfolio."/>
-            <meta name="twitter:card" content="summary"/>
-            <meta name="twitter:creator" content="@Alessandro_Info"/>
-            <meta name="theme-color" content="#032a2e"/>
-        </Head>
         <body>
         {children}
         </body>
